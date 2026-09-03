2c5bc25728bbbc10f979ded1a89d0877b733a0c3
kushima8 <58157342+kushima8@users.noreply.github.com>
2026-09-03 12:02:25 +0900

    fix

--- drivers/pmw3360/pmw3360.c
+++ drivers/pmw3360/pmw3360.c
@@ -23,8 +23,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "srom_0x81.c"
 
 #define PMW3360_SPI_MODE 3
-//#define PMW3360_SPI_DIVISOR (F_CPU / PMW3360_CLOCKS)
-#define PMW3360_SPI_DIVISOR 64
+#ifndef PMW3360_SPI_DIVISOR
+#    ifdef __AVR__
+#        define PMW3360_SPI_DIVISOR (F_CPU / PMW3360_CLOCKS)
+#    else
+#        define PMW3360_SPI_DIVISOR 64
+#    endif
+#endif
 #define PMW3360_CLOCKS 2000000
 
 static const pin_t pins[] = PMW3360_NCS_PINS;
